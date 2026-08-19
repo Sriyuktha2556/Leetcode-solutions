a012a1d3c7853810ca9486296f5ddc425cb25ff7
Sriyuktha Sravani <152289010+Sriyuktha2556@users.noreply.github.com>
2026-08-19 10:52:55 +0530

    Time: 0 ms (100%), Space: 41.9 MB (70.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -4,7 +4,7 @@ class Solution {
         int[]dp=new int[n+1];
         dp[1]=dp[2]=1;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-2]+dp[i-1];
+            dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
     }
